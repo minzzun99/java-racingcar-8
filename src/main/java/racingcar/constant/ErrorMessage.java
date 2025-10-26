4a22a6d7863fd6f2f4cd3f05292aeffa2b1c7ce1
minzzun99 <skg06123@naver.com>
2025-10-26 23:04:02 +0900

    refactor: 빈 값 입력 시 예외 메시지 수정
    - "자동차의 이름을 입력해주세요." -> "값이 입력되지 않았습니다."로 변경

--- src/main/java/racingcar/constant/ErrorMessage.java
+++ src/main/java/racingcar/constant/ErrorMessage.java
@@ -3,7 +3,7 @@ package racingcar.constant;
 public enum ErrorMessage {
     INVALID_NUMBER_FORMAT("2,147,483,647 이하의 양수를 입력해주세요."),
     INVALID_ROUND_RANGE("시도할 횟수는 1 이상의 숫자를 입력해주세요."),
-    NULL_OR_EMPTY_INPUT("자동차의 이름을 입력해주세요."),
+    NULL_OR_EMPTY_INPUT("값이 입력되지 않았습니다."),
     EMPTY_CAR_NAME_INPUT("자동차 이름에 빈 값이 포함되어 있습니다."),
     INVALID_CAR_NAME_LENGTH("자동차의 이름은 %d자 이하로 입력해주세요.");
 

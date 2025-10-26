c725cd19cb6e405637651acfc4da208e4cf2a369
minzzun99 <skg06123@naver.com>
2025-10-26 21:23:53 +0900

    feat: Application 클래스 구현
    - main 메서드에서 RacingController 실행
    - Console.close()로 리소스 관리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.controller.RacingController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            RacingController racingController = new RacingController();
+            racingController.start();
+        } finally {
+            Console.close();
+        }
     }
 }
